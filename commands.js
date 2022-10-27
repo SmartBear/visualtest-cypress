a7cbc7e6b1fb5e3d4044f8a8fe64b49f94091e0e
trevor.nelms <trevor.nelms@smartbear.com>
2022-10-27 12:05:01 -0600

    SBVT-558: Formatting changes

--- commands.js
+++ commands.js
@@ -78,21 +78,18 @@ let takeScreenshot = (element, name, modifiedOptions) => {
                     userAgent();
                     domCapture();
                     picFileFormat();
-                    (() => {
-                        cy.task('logger', {type: 'trace', message: `After fullpage cy.screenshot('${name}')`});
-                        cy.window()
-                            .then((win) => {
-                                cy.task('logger', {type: 'trace', message: `document.body.style.transform='${initialPageState.transform}'`});
-                                cy.task('logger', {type: 'trace',message: `window.scrollTo(${initialPageState.scrollX}, ${initialPageState.scrollY})`});
-                                cy.task('logger', {type: 'trace',message: `document.body.style.overflow='${initialPageState.overflow}'`});
+                    cy.window()
+                        .then((win) => {
+                            cy.task('logger', {type: 'trace', message: `document.body.style.transform='${initialPageState.transform}'`});
+                            cy.task('logger', {type: 'trace',message: `window.scrollTo(${initialPageState.scrollX}, ${initialPageState.scrollY})`});
+                            cy.task('logger', {type: 'trace',message: `document.body.style.overflow='${initialPageState.overflow}'`});
 
-                                win.eval(`document.body.style.transform='${initialPageState.transform}'`)
-                                win.eval(`window.scrollTo(${initialPageState.scrollX}, ${initialPageState.scrollY})`)
-                                win.eval(`document.body.style.overflow='${initialPageState.overflow}'`)
+                            win.eval(`document.body.style.transform='${initialPageState.transform}'`)
+                            win.eval(`window.scrollTo(${initialPageState.scrollX}, ${initialPageState.scrollY})`)
+                            win.eval(`document.body.style.overflow='${initialPageState.overflow}'`)
 
-                                cy.task('logger', {type: 'trace', message: `After fullpage cy.screenshot('${name}'), and website state is back to before fullpage.`});
-                            })
-                    })();
+                            cy.task('logger', {type: 'trace', message: `After fullpage cy.screenshot('${name}')`});
+                        })
                 });
             })
     }
