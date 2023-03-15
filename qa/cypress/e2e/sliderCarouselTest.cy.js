43e2418060ff97bb2d674d8f5c47126a42bcbc7b
trevor.nelms <trevor.nelms@smartbear.com>
2023-03-15 09:30:04 -0600

    SBVT-1153: adding test file

--- qa/cypress/e2e/sliderCarouselTest.cy.js
+++ qa/cypress/e2e/sliderCarouselTest.cy.js
@@ -7,11 +7,16 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe(`running example 1`, () => {
     beforeEach('visit', () => {
         cy.visit(`https://smartbear.github.io/visual-testing-example-website/Example1/Original/index.html`)
+        cy.window()
+            .then((win) => {
+                cy.readFile("./exampleFreezePage.js").then((str) => {
+                    win.eval(str)
+                })
+            })
     })
     it('should take a fullpage sbvtCapture', function () {
-        cy.sbvtCapture('1example 1 lazyloaded 750ms', {
+        cy.sbvtCapture('example 1 lazyloaded 750ms', {
             lazyload: 750,
-            script: "./exampleFreezePage.js"
         })
     });
 })
@@ -19,11 +24,16 @@ describe(`running example 1`, () => {
 describe(`running example 2`, () => {
     beforeEach('visit', () => {
         cy.visit(`https://smartbear.github.io/visual-testing-example-website/Example2/Original/index.html`)
+        cy.window()
+            .then((win) => {
+                cy.readFile("./exampleFreezePage.js").then((str) => {
+                    win.eval(str)
+                })
+            })
     })
     it('should take a fullpage sbvtCapture', function () {
         cy.sbvtCapture('example 2 lazyloaded 750ms', {
             lazyload: 750,
-            script: "./exampleFreezePage.js"
         })
     });
 })
@@ -31,11 +41,16 @@ describe(`running example 2`, () => {
 describe(`running example 3`, () => {
     beforeEach('visit', () => {
         cy.visit(`https://smartbear.github.io/visual-testing-example-website/Example3/Original/index.html`)
+        cy.window()
+            .then((win) => {
+                cy.readFile("./exampleFreezePage.js").then((str) => {
+                    win.eval(str)
+                })
+            })
     })
     it('should take a fullpage sbvtCapture', function () {
-        cy.sbvtCapture('example 3 lazyloaded 750ms-2', {
+        cy.sbvtCapture('example 3 lazyloaded 750ms', {
             lazyload: 750,
-            script: "./exampleFreezePage.js"
         })
     });
 })
@@ -43,11 +58,16 @@ describe(`running example 3`, () => {
 describe(`running example 4`, () => {
     beforeEach('visit', () => {
         cy.visit(`https://smartbear.github.io/visual-testing-example-website/Example4/Original/index.html`)
+        cy.window()
+            .then((win) => {
+                cy.readFile("./exampleFreezePage.js").then((str) => {
+                    win.eval(str)
+                })
+            })
     })
     it('should take a fullpage sbvtCapture', function () {
         cy.sbvtCapture('example 4 lazyloaded 750ms', {
             lazyload: 750,
-            script: "./exampleFreezePage.js"
         })
     });
 })
@@ -55,11 +75,16 @@ describe(`running example 4`, () => {
 describe(`running example 5`, () => {
     beforeEach('visit', () => {
         cy.visit(`https://smartbear.github.io/visual-testing-example-website/Example5/Original/index.html`)
+        cy.window()
+            .then((win) => {
+                cy.readFile("./exampleFreezePage.js").then((str) => {
+                    win.eval(str)
+                })
+            })
     })
     it('should take a fullpage sbvtCapture', function () {
         cy.sbvtCapture('example 5 lazyloaded 750ms', {
             lazyload: 750,
-            script: "./exampleFreezePage.js"
         })
     });
 })
@@ -67,11 +92,16 @@ describe(`running example 5`, () => {
 describe(`running example 6`, () => {
     beforeEach('visit', () => {
         cy.visit(`https://smartbear.github.io/visual-testing-example-website/Example6/Original/index.html`)
+        cy.window()
+            .then((win) => {
+                cy.readFile("./exampleFreezePage.js").then((str) => {
+                    win.eval(str)
+                })
+            })
     })
     it('should take a fullpage sbvtCapture', function () {
         cy.sbvtCapture('example 6 lazyloaded 750ms', {
             lazyload: 750,
-            script: "./exampleFreezePage.js"
         })
     });
 });
