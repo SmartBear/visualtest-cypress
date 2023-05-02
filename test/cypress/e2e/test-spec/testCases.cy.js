7c4ca9e31788c6326e41b9174989bd774a12fcac
Trevor Nelms <trevornelms@outlook.com>
2023-05-02 11:27:04 -0600

    SBVT-1387: clear ignoredElements after each capture (#82)
    * SBVT-1387: clearing the window before each run
    
    * SBVT-1387: update the tests for ignoredElements
    
    * SBVT-1387: update version and allow for npm test in main
    
    * SBVT-1387: fixing exampleFreezeCarousel.js script
    
    ---------
    
    Co-authored-by: trevor.nelms <trevor.nelms@smartbear.com>

--- test/cypress/e2e/test-spec/testCases.cy.js
+++ test/cypress/e2e/test-spec/testCases.cy.js
@@ -102,7 +102,7 @@ const flattenTree = dom => {
     return flat;
 }
 
-Cypress.on('uncaught:exception', (err, runnable) => {
+Cypress.on('uncaught:exception', () => {
     // returning false here prevents Cypress from
     // failing the test
     return false
@@ -115,7 +115,7 @@ testCases.forEach(currentTestCase => {
         it(`should take sbvtCapture`, () => {
             cy.visit(currentTestCase.url).then(() => {
                 currentTestCase.options.saveDOM = true;
-                cy.wait(1000);
+                cy.wait(1500);
                 cy.window()
                     .then((win) => {
                         cy.readFile("./exampleFreezeCarousel.js").then((str) => {
@@ -128,6 +128,7 @@ testCases.forEach(currentTestCase => {
             })
         })
         it(`dom should have correct data`, () => {
+            assert(dataFromTest.dom, 'DOM missing from result')
             assert(dataFromTest.dom.error === false,'DOM capture has an error');
             assert(dataFromTest.dom.fullpage.width && dataFromTest.dom.fullpage.height,'DOM capture doesnt have fullpage width and height');
             assert(dataFromTest.dom.viewport.width && dataFromTest.dom.viewport.height,'DOM capture doesnt have viewport width and height');
@@ -136,7 +137,6 @@ testCases.forEach(currentTestCase => {
         })
         if (currentTestCase.options.ignoreElements) {
             it(`check that the dom has the cssSelectors in the ignoredElements`, () => {
-                assert(dataFromTest.dom, 'dom missing from result')
                 assert(dataFromTest.dom.ignoredElementsData, 'ignoredElementsData missing from dom');
                 const ignoredElements = dataFromTest.dom.ignoredElementsData;
                 assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
@@ -155,6 +155,20 @@ testCases.forEach(currentTestCase => {
                 assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
                 assert(ignoredElements.length > 0, 'ignoredElements on image API result was an empty array');
                 assert(currentTestCase.options.ignoreElements.every(selector => ignoredElements.some(el => el.cssSelector === selector)), 'ignoreElements cssSelectors requested did not match found ignoredElements cssSelectors');
+            })
+        } else {
+            it(`check that the dom.ignoredElements is empty`, () => {
+                assert(dataFromTest.dom.ignoredElementsData, 'ignoredElementsData missing from dom');
+                const ignoredElements = dataFromTest.dom.ignoredElementsData;
+                assert(Array.isArray(ignoredElements), 'ignoredElementsData on image API result was not an array');
+                assert(ignoredElements.length === 0, 'ignoredElementsData is not empty');
+            })
+            it(`check that the image API result on ignoredElements is empty`, () => {
+                assert(dataFromTest.imageApiResult, 'imageApiResult missing from API result');
+                assert(dataFromTest.imageApiResult.ignoredElements, 'ignoredElements property missing from API result');
+                const ignoredElements = dataFromTest.imageApiResult.ignoredElements;
+                assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
+                assert(ignoredElements.length === 0, 'ignoredElements on image API result was not an empty array');
             })
         }
         if (currentTestCase.validation) {
