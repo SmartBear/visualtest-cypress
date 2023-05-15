8ec50b9aa08427f3db9a9d8da7d7d7fccca751a1
trevor.nelms <trevor.nelms@smartbear.com>
2023-05-15 15:22:01 -0600

    SBVT-1440: Add resolutions to functional tests

--- test/cypress/e2e/test-spec/test_element-screenshot.cy.js
+++ test/cypress/e2e/test-spec/test_element-screenshot.cy.js
@@ -1,3 +1,4 @@
+const resolutions = ['1024x768', '1496x967', '1920x1080']
 const testCases = [
     {
         'name': 'Example1-Original-Product-Item',
@@ -44,44 +45,48 @@ Cypress.on('uncaught:exception', () => {
 })
 
 const insertCustomFreezeScript = true;
-testCases.forEach(currentTestCase => {
-    let dataFromTest;
-    describe(getDescribeTitle(Cypress.spec.name, currentTestCase), () => {
-        it(`should take sbvtCapture`, () => {
-            cy.visit(currentTestCase.url).then(() => {
-                currentTestCase.options ? '' : currentTestCase.options = {}
-                currentTestCase.options.saveDOM = true;
-                cy.wait(1500);
-                if (currentTestCase.actions && currentTestCase.actions.length > 0) {
-                    if (currentTestCase.actions[0].action === 'click') {
-                        cy.get(currentTestCase.actions[0].cssSelector).click();
-                        cy.get(currentTestCase.cssSelector).sbvtCapture(currentTestCase.name, currentTestCase.options).then((data) => {
-                            dataFromTest = data;
-                        })
-                    } else if (currentTestCase.actions[0].action === 'hover') {
-                        //cypress does not handle hovers
-                    }
-                } else {
-                    cy.window()
-                        .then((win) => {
-                            cy.readFile("./exampleFreezeCarousel.js").then((str) => {
-                                if (insertCustomFreezeScript) win.eval(str);
-                                cy.get(currentTestCase.cssSelector).sbvtCapture(currentTestCase.name, currentTestCase.options).then((data) => {
-                                    dataFromTest = data;
+resolutions.forEach(widthHeight => {
+    const [width, height] = widthHeight.split('x');
+    testCases.forEach(currentTestCase => {
+        let dataFromTest;
+        describe(getDescribeTitle(Cypress.spec.name, currentTestCase), () => {
+            it(`should take sbvtCapture`, () => {
+                cy.viewport(parseInt(width), parseInt(height))
+                cy.visit(currentTestCase.url).then(() => {
+                    currentTestCase.options ? '' : currentTestCase.options = {}
+                    currentTestCase.options.saveDOM = true;
+                    cy.wait(1500);
+                    if (currentTestCase.actions && currentTestCase.actions.length > 0) {
+                        if (currentTestCase.actions[0].action === 'click') {
+                            cy.get(currentTestCase.actions[0].cssSelector).click();
+                            cy.get(currentTestCase.cssSelector).sbvtCapture(currentTestCase.name, currentTestCase.options).then((data) => {
+                                dataFromTest = data;
+                            })
+                        } else if (currentTestCase.actions[0].action === 'hover') {
+                            //cypress does not handle hovers
+                        }
+                    } else {
+                        cy.window()
+                            .then((win) => {
+                                cy.readFile("./exampleFreezeCarousel.js").then((str) => {
+                                    if (insertCustomFreezeScript) win.eval(str);
+                                    cy.get(currentTestCase.cssSelector).sbvtCapture(currentTestCase.name, currentTestCase.options).then((data) => {
+                                        dataFromTest = data;
+                                    })
                                 })
                             })
-                        })
-                }
+                    }
+                })
             })
-        })
-        it(`dom should have correct data`, () => {
-            assert(dataFromTest.dom, 'DOM is missing');
-            assert(dataFromTest.dom.error === false, 'DOM capture has an error');
-            assert(dataFromTest.dom.fullpage.width && dataFromTest.dom.fullpage.height, 'DOM capture doesnt have fullpage width and height');
-            assert(dataFromTest.dom.viewport.width && dataFromTest.dom.viewport.height, 'DOM capture doesnt have viewport width and height');
-            assert(dataFromTest.dom.devicePixelRatio >= 1, 'DOM capture invalid devicePixelRatio');
-            assert(dataFromTest.dom.dom.length >= 1, 'DOM elements missing');
-            assert(dataFromTest.imageApiResult.imageType === 'element', `DOM screenshotType invalid: ${dataFromTest.dom.screenshotResult}`);
+            it(`dom should have correct data`, () => {
+                assert(dataFromTest.dom, 'DOM is missing');
+                assert(dataFromTest.dom.error === false, 'DOM capture has an error');
+                assert(dataFromTest.dom.fullpage.width && dataFromTest.dom.fullpage.height, 'DOM capture doesnt have fullpage width and height');
+                assert(dataFromTest.dom.viewport.width && dataFromTest.dom.viewport.height, 'DOM capture doesnt have viewport width and height');
+                assert(dataFromTest.dom.devicePixelRatio >= 1, 'DOM capture invalid devicePixelRatio');
+                assert(dataFromTest.dom.dom.length >= 1, 'DOM elements missing');
+                assert(dataFromTest.imageApiResult.imageType === 'element', `DOM screenshotType invalid: ${dataFromTest.dom.screenshotResult}`);
+            })
         })
     })
 })
--- test/cypress/e2e/test-spec/test_fullpage-screenshot.cy.js
+++ test/cypress/e2e/test-spec/test_fullpage-screenshot.cy.js
@@ -1,3 +1,4 @@
+const resolutions = ['1024x768', '1496x967', '1920x1080']
 const testCases = [
     {
         'name': 'Example1-Original',
@@ -83,83 +84,87 @@ Cypress.on('uncaught:exception', () => {
 })
 
 const insertCustomFreezeScript = true;
-testCases.forEach(currentTestCase => {
-    let dataFromTest;
-    describe(getDescribeTitle(Cypress.spec.name, currentTestCase), () => {
-        it(`should take sbvtCapture`, () => {
-            cy.visit(currentTestCase.url).then(() => {
-                currentTestCase.options.saveDOM = true;
-                cy.wait(1500);
-                cy.window()
-                    .then((win) => {
-                        cy.readFile("./exampleFreezeCarousel.js").then((str) => {
-                            if (insertCustomFreezeScript) win.eval(str);
-                            cy.sbvtCapture(currentTestCase.name, currentTestCase.options).then((data) => {
-                                dataFromTest = data;
+resolutions.forEach(widthHeight => {
+    const [width, height] = widthHeight.split('x');
+    testCases.forEach(currentTestCase => {
+        let dataFromTest;
+        describe(getDescribeTitle(Cypress.spec.name, currentTestCase), () => {
+            it(`should take sbvtCapture`, () => {
+                cy.viewport(parseInt(width), parseInt(height))
+                cy.visit(currentTestCase.url).then(() => {
+                    currentTestCase.options.saveDOM = true;
+                    cy.wait(1500);
+                    cy.window()
+                        .then((win) => {
+                            cy.readFile("./exampleFreezeCarousel.js").then((str) => {
+                                if (insertCustomFreezeScript) win.eval(str);
+                                cy.sbvtCapture(currentTestCase.name, currentTestCase.options).then((data) => {
+                                    dataFromTest = data;
+                                })
                             })
                         })
-                    })
-            })
-        })
-        it(`dom should have correct data`, () => {
-            assert(dataFromTest.dom, 'DOM missing from result')
-            assert(dataFromTest.dom.error === false, 'DOM capture has an error');
-            assert(dataFromTest.dom.fullpage.width && dataFromTest.dom.fullpage.height, 'DOM capture doesnt have fullpage width and height');
-            assert(dataFromTest.dom.viewport.width && dataFromTest.dom.viewport.height, 'DOM capture doesnt have viewport width and height');
-            assert(dataFromTest.dom.devicePixelRatio >= 1, 'DOM capture invalid devicePixelRatio');
-            assert(dataFromTest.dom.dom.length >= 1, 'DOM elements missing');
-        })
-        if (currentTestCase.options.ignoreElements) {
-            it(`check that the dom has the cssSelectors in the ignoredElements`, () => {
-                assert(dataFromTest.dom, 'dom missing from result')
-                assert(dataFromTest.dom.ignoredElementsData, 'ignoredElementsData missing from dom');
-                const ignoredElements = dataFromTest.dom.ignoredElementsData;
-                assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
-                assert(ignoredElements.length > 0, 'ignoredElementsData is an empty array');
-                assert(currentTestCase.options.ignoreElements.every(selector => ignoredElements.some(el => el.cssSelector === selector)), 'ignoreElements cssSelectors requested did not match found ignoredElements cssSelectors');
-            })
-            it(`filter through the flat dom and make sure there are some cases of ignore: true`, () => {
-                const flatDom = flattenDom(dataFromTest.dom.dom[0])
-                const elementsToBeIgnored = flatDom.filter(el => el.ignore)
-                assert(elementsToBeIgnored.length >= currentTestCase.options.ignoreElements.length, 'There are less elements ignored than testCase ignoreElements.length');
-            })
-            it(`check that the image API result has the cssSelectors in the ignoredElements`, () => {
-                assert(dataFromTest.imageApiResult, 'imageApiResult missing from API result');
-                assert(dataFromTest.imageApiResult.ignoredElements, 'ignoredElements property missing from API result');
-                const ignoredElements = dataFromTest.imageApiResult.ignoredElements;
-                assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
-                assert(ignoredElements.length > 0, 'ignoredElements on image API result was an empty array');
-                assert(currentTestCase.options.ignoreElements.every(selector => ignoredElements.some(el => el.cssSelector === selector)), 'ignoreElements cssSelectors requested did not match found ignoredElements cssSelectors');
-            })
-        } else {
-            it(`check that the dom.ignoredElements is empty`, () => {
-                assert(dataFromTest.dom.ignoredElementsData, 'ignoredElementsData missing from dom');
-                const ignoredElements = dataFromTest.dom.ignoredElementsData;
-                assert(Array.isArray(ignoredElements), 'ignoredElementsData on image API result was not an array');
-                assert(ignoredElements.length === 0, 'ignoredElementsData is not empty');
-            })
-            it(`check that the image API result on ignoredElements is empty`, () => {
-                assert(dataFromTest.imageApiResult, 'imageApiResult missing from API result');
-                assert(dataFromTest.imageApiResult.ignoredElements, 'ignoredElements property missing from API result');
-                const ignoredElements = dataFromTest.imageApiResult.ignoredElements;
-                assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
-                assert(ignoredElements.length === 0, 'ignoredElements on image API result was not an empty array');
-            })
-        }
-        if (currentTestCase.validation) {
-            it(`dom should prove lazyload is working`, () => {
-                const flatDom = flattenDom(dataFromTest.dom.dom[0])
-                // go through all the elements in the testCases JSON
-                currentTestCase.validation.elements.forEach(currentElementOnTestcase => {
-                    // filter through the flatDom for cssSelectors on it, that match the testcase
-                    const matchingElementsOnDom = flatDom.filter(el => el.attrs && el.attrs.class && el.attrs.class.includes(currentElementOnTestcase.class))
-                    assert(matchingElementsOnDom.length === currentElementOnTestcase.count, `DOM did not have ${currentElementOnTestcase.count} elements matching class: "${currentElementOnTestcase.class}", had ${matchingElementsOnDom.length}.`);
-                    matchingElementsOnDom.forEach(matchedElementOnDom => {
-                        assert(matchedElementOnDom.styles.visibility === currentElementOnTestcase.styles.visibility, `Element "${matchedElementOnDom.attrs.class}" had "${matchedElementOnDom.styles.visibility}" visibility.`);
-                    })
                 })
             })
-        }
+            it(`dom should have correct data`, () => {
+                assert(dataFromTest.dom, 'DOM missing from result')
+                assert(dataFromTest.dom.error === false, 'DOM capture has an error');
+                assert(dataFromTest.dom.fullpage.width && dataFromTest.dom.fullpage.height, 'DOM capture doesnt have fullpage width and height');
+                assert(dataFromTest.dom.viewport.width && dataFromTest.dom.viewport.height, 'DOM capture doesnt have viewport width and height');
+                assert(dataFromTest.dom.devicePixelRatio >= 1, 'DOM capture invalid devicePixelRatio');
+                assert(dataFromTest.dom.dom.length >= 1, 'DOM elements missing');
+            })
+            if (currentTestCase.options.ignoreElements) {
+                it(`check that the dom has the cssSelectors in the ignoredElements`, () => {
+                    assert(dataFromTest.dom, 'dom missing from result')
+                    assert(dataFromTest.dom.ignoredElementsData, 'ignoredElementsData missing from dom');
+                    const ignoredElements = dataFromTest.dom.ignoredElementsData;
+                    assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
+                    assert(ignoredElements.length > 0, 'ignoredElementsData is an empty array');
+                    assert(currentTestCase.options.ignoreElements.every(selector => ignoredElements.some(el => el.cssSelector === selector)), 'ignoreElements cssSelectors requested did not match found ignoredElements cssSelectors');
+                })
+                it(`filter through the flat dom and make sure there are some cases of ignore: true`, () => {
+                    const flatDom = flattenDom(dataFromTest.dom.dom[0])
+                    const elementsToBeIgnored = flatDom.filter(el => el.ignore)
+                    assert(elementsToBeIgnored.length >= currentTestCase.options.ignoreElements.length, 'There are less elements ignored than testCase ignoreElements.length');
+                })
+                it(`check that the image API result has the cssSelectors in the ignoredElements`, () => {
+                    assert(dataFromTest.imageApiResult, 'imageApiResult missing from API result');
+                    assert(dataFromTest.imageApiResult.ignoredElements, 'ignoredElements property missing from API result');
+                    const ignoredElements = dataFromTest.imageApiResult.ignoredElements;
+                    assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
+                    assert(ignoredElements.length > 0, 'ignoredElements on image API result was an empty array');
+                    assert(currentTestCase.options.ignoreElements.every(selector => ignoredElements.some(el => el.cssSelector === selector)), 'ignoreElements cssSelectors requested did not match found ignoredElements cssSelectors');
+                })
+            } else {
+                it(`check that the dom.ignoredElements is empty`, () => {
+                    assert(dataFromTest.dom.ignoredElementsData, 'ignoredElementsData missing from dom');
+                    const ignoredElements = dataFromTest.dom.ignoredElementsData;
+                    assert(Array.isArray(ignoredElements), 'ignoredElementsData on image API result was not an array');
+                    assert(ignoredElements.length === 0, 'ignoredElementsData is not empty');
+                })
+                it(`check that the image API result on ignoredElements is empty`, () => {
+                    assert(dataFromTest.imageApiResult, 'imageApiResult missing from API result');
+                    assert(dataFromTest.imageApiResult.ignoredElements, 'ignoredElements property missing from API result');
+                    const ignoredElements = dataFromTest.imageApiResult.ignoredElements;
+                    assert(Array.isArray(ignoredElements), 'ignoredElements on image API result was not an array');
+                    assert(ignoredElements.length === 0, 'ignoredElements on image API result was not an empty array');
+                })
+            }
+            if (currentTestCase.validation) {
+                it(`dom should prove lazyload is working`, () => {
+                    const flatDom = flattenDom(dataFromTest.dom.dom[0])
+                    // go through all the elements in the testCases JSON
+                    currentTestCase.validation.elements.forEach(currentElementOnTestcase => {
+                        // filter through the flatDom for cssSelectors on it, that match the testcase
+                        const matchingElementsOnDom = flatDom.filter(el => el.attrs && el.attrs.class && el.attrs.class.includes(currentElementOnTestcase.class))
+                        assert(matchingElementsOnDom.length === currentElementOnTestcase.count, `DOM did not have ${currentElementOnTestcase.count} elements matching class: "${currentElementOnTestcase.class}", had ${matchingElementsOnDom.length}.`);
+                        matchingElementsOnDom.forEach(matchedElementOnDom => {
+                            assert(matchedElementOnDom.styles.visibility === currentElementOnTestcase.styles.visibility, `Element "${matchedElementOnDom.attrs.class}" had "${matchedElementOnDom.styles.visibility}" visibility.`);
+                        })
+                    })
+                })
+            }
+        })
     })
 })
 
