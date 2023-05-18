992cd6e9de7582d75a87939003c4d3712de66d5e
trevor.nelms <trevor.nelms@smartbear.com>
2023-05-18 14:51:39 -0600

    SBVT-1465-1: formatting

--- bin/setup.js
+++ bin/setup.js
@@ -2,25 +2,29 @@
 
 const fs = require('fs');
 const path = require('path');
-const chalk = require('chalk')
+const chalk = require('chalk');
 const cwd = process.cwd();
 
 const pluginRequire = `\nrequire('@smartbear/visualtest-cypress')(module)`;
 const commandsImport = `\nimport '@smartbear/visualtest-cypress/commands'`;
 const vtConfContent = `module.exports = {\n\tprojectToken: 'PROJECT_TOKEN',\n}`;
-const jsonData = `"chromeWebSecurity": false`
+const jsonData = `"chromeWebSecurity": false`;
 //TODO add try catch
-const packageFile = fs.readFileSync(path.resolve(path.dirname(require.resolve('cypress', {paths: [cwd]})), 'package.json'))
+const packageFile = fs.readFileSync(path.resolve(path.dirname(require.resolve('cypress', {paths: [cwd]})), 'package.json'));
 const usersCypress = JSON.parse(packageFile.toString());
 let error = false;
 
 let checkForOlderVersion = () => {
-    if (usersCypress.version.split('.')[0] < 7 ) {
-        // Note as of now this is not supported because fs.rmSync is not on the Node version before this Cypress version
-        console.log(chalk.redBright(`Detected Cypress ${usersCypress.version}`))
-        console.log(chalk.green(`Only Versions 7.0.0+ are supported`));
-        error = true
-    } else if ((usersCypress.version.split('.')[0] = 7 && usersCypress.version.split('.')[1] < 4)) {
+    if (usersCypress.version.split('.')[0] < 7 || usersCypress.version.split('.')[0] <= 7 && usersCypress.version.split('.')[1] < 4) {
+        // Note as of now this is not supported because cy.request cannot send blobs in previous versions
+        console.log(chalk.redBright(`Detected Cypress ${usersCypress.version}`));
+        console.log(chalk.green(`Only Cypress 7.4.0+ is supported`));
+        error = true;
+    }
+    /**
+    if we decide to support older versions commenting out for now
+
+    else if ((usersCypress.version.split('.')[0] = 7 && usersCypress.version.split('.')[1] < 4)) {
         const filePath = path.resolve(process.cwd(), 'cypress.json');
         if (fs.existsSync(filePath)) { //file exists
             const fileContent = fs.readFileSync(filePath, 'utf-8');
@@ -47,15 +51,16 @@ let checkForOlderVersion = () => {
             console.log(chalk.dim(`Read about it here: https://docs.cypress.io/guides/guides/web-security\n`))
         }
     }
-}
+     **/
+};
 let setupVTConf = () => {
     const filePath = `${process.cwd()}/visualTest.config.js`;
 
-    if (fs.existsSync(filePath)){ //file exists
+    if (fs.existsSync(filePath)) { //file exists
         console.log(chalk.blue(`visualTest.config.js found.`));
         const fileContent = fs.readFileSync(filePath, 'utf-8');
 
-        if (fileContent.toString().includes('projectToken') && fileContent.toString().includes('module.exports')){ //vtConf boilerplate looks good
+        if (fileContent.toString().includes('projectToken') && fileContent.toString().includes('module.exports')) { //vtConf boilerplate looks good
 
             if (fileContent.toString().includes(`projectToken: 'PROJECT_TOKEN'`)) { //user has not entered their own projectToken
                 console.log(chalk.underline.yellow('Please enter your projectToken in visualTest.config.js.'));
@@ -67,12 +72,14 @@ let setupVTConf = () => {
             console.log(chalk.underline.yellow('Please enter your projectToken in visualTest.config.js.'));
         }
     } else { //file does not exist, so create the file and import boilerplate
-        fs.appendFile('visualTest.config.js', vtConfContent, function (err) { if (err) throw err });
+        fs.appendFile('visualTest.config.js', vtConfContent, function (err) {
+            if (err) throw err;
+        });
         console.log(chalk.green('visualTest.config.js has been created.'));
         process.stdout.write(chalk.yellow('Please enter your projectToken'));
         console.log(chalk.green(' in visualTest.config.js'));
     }
-}
+};
 let setupCommands = () => {
     let filePath;
     if (usersCypress.version.split('.')[0] >= 10) {
@@ -84,19 +91,22 @@ let setupCommands = () => {
     try {
         fileContent = fs.readFileSync(filePath, 'utf-8');
     } catch (err) {
-        error = true
-        console.log(chalk.bgRedBright(`Cypress e2e.js file not found, this is most likely due to Cypress not being setup yet (npx cypress open).`));
-        console.log(chalk.grey(`\t${err}`));
-        return
+        error = true;
+        console.log(chalk.red(`Cypress e2e.js file not found, this is most likely due to Cypress not being setup yet, please run: `));
+        console.log("");
+        console.log("\tnpx cypress open");
+        console.log("");
+        console.log(chalk.grey(`${err}`));
+        return;
     }
 
-    if (fileContent.toString().includes(commandsImport)){
+    if (fileContent.toString().includes(commandsImport)) {
         console.log(chalk.blue(`Commands already installed.`));
     } else {
         fs.writeFileSync(filePath, fileContent.replace(/([\s\S])$/, `$1${commandsImport}`));
         console.log(chalk.green(`Commands installed.`));
     }
-}
+};
 let setupPlugin = () => {
     let filePath;
     if (usersCypress.version.split('.')[0] >= 10) {
@@ -108,21 +118,24 @@ let setupPlugin = () => {
     try {
         fileContent = fs.readFileSync(filePath, 'utf-8');
     } catch (err) {
-        error = true
-        console.log(chalk.bgRedBright(`Cypress cypress.config.js file not found, this is most likely due to Cypress not being setup yet (npx cypress open).`));
-        console.log(chalk.grey(`\t${err}`));
-        return
+        error = true;
+        console.log(chalk.red(`Cypress cypress.config.js file not found, this is most likely due to Cypress not being setup yet, please run: `));
+        console.log("");
+        console.log("npx cypress open");
+        console.log("");
+        console.log(chalk.grey(`${err}`));
+        return;
     }
 
-    if (fileContent.toString().includes(pluginRequire)){
+    if (fileContent.toString().includes(pluginRequire)) {
         console.log(chalk.blue(`Plugin already installed.`));
     } else {
         fs.writeFileSync(filePath, fileContent.replace(/([\s\S])$/, `$1${pluginRequire}`));
         console.log(chalk.green(`Plugin installed.`));
     }
-}
+};
 
-if (!error) checkForOlderVersion()
-if (!error) setupCommands()
-if (!error) setupPlugin()
-if (!error) setupVTConf()
+if (!error) checkForOlderVersion();
+if (!error) setupCommands();
+if (!error) setupPlugin();
+if (!error) setupVTConf();
