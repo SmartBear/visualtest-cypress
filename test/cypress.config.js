5a8fea131d3d92ebfcd41674d8aaac6857fec6c7
trevor.nelms <trevor.nelms@smartbear.com>
2023-05-22 08:47:23 -0600

    updating the yaml file

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -5,6 +5,7 @@ const generateReports = require("./utils/generateReports")
 
 
 module.exports = defineConfig({
+    projectId: "x6we8y",
     // viewportWidth: 1920,
     // viewportHeight: 1080,
     reporterOptions: {
