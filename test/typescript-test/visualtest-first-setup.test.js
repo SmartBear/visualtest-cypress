1a6453da108ff6b24a05f388ff41109ab67b1f75
trevor.nelms <trevor.nelms@smartbear.com>
2023-05-26 13:44:18 -0600

    SBVT-1486: test

--- test/typescript-test/visualtest-first-setup.test.js
+++ test/typescript-test/visualtest-first-setup.test.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 describe('Visual Test Setup', () => {
     test('Console logs', () => {
         const expectedLogs = `Commands installed.
@@ -6,7 +8,7 @@ TypeScript import statement added.  Filepath: cypress/support/index.d.ts
 visualTest.config.js has been created.
 Please enter your projectToken in visualTest.config.js`;
 
-        const actualLogs = process.stdout.write.mock.calls.join('');
-        expect(actualLogs).toEqual(expectedLogs);
+        const fileContent = fs.readFileSync("visualtest-logs.txt", 'utf-8');
+        expect(fileContent).toEqual(expectedLogs);
     });
 });
