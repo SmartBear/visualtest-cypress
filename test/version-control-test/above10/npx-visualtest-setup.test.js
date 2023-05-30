7484d6ce13b806463cc9ff67226598d5420d76df
trevor.nelms <trevor.nelms@smartbear.com>
2023-05-30 11:36:11 -0600

    SBVT-1491: npx visualtest checks

--- test/version-control-test/above10/npx-visualtest-setup.test.js
+++ test/version-control-test/above10/npx-visualtest-setup.test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 test('Check visualtest-first-log.txt', () => {
     const fileContents = fs.readFileSync('visualtest-first-logs.txt', 'utf-8');
 
-    expect(fileContents).toContain('TypeScript detected - this currently in beta.');
+    expect(fileContents).not.toContain('TypeScript detected - this currently in beta.');
     expect(fileContents).toContain('Commands installed.');
     expect(fileContents).toContain('Plugin installed.');
     // expect(fileContents).toContain('TypeScript import statement added.	 Filepath: cypress/support/index.d.ts'); //issue with the spacing on expect and actual
@@ -16,7 +16,7 @@ test('Check visualtest-first-log.txt', () => {
 test('Check visualtest-second-log.txt', () => {
     const fileContents = fs.readFileSync('visualtest-second-logs.txt', 'utf-8');
 
-    expect(fileContents).toContain('TypeScript detected - this currently in beta.');
+    expect(fileContents).not.toContain('TypeScript detected - this currently in beta.');
     expect(fileContents).toContain('Commands already installed.');
     expect(fileContents).toContain('Plugin already installed.');
     expect(fileContents).toContain('visualTest.config.js found.');
