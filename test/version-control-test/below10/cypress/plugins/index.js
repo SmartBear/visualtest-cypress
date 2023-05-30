6ea34e13fde67134fd3901fc3a8640492bab1bf4
trevor.nelms <trevor.nelms@smartbear.com>
2023-05-30 11:44:32 -0600

    SBVT-1491: working on below10: install jest again and remove the setup commmands

--- test/version-control-test/below10/cypress/plugins/index.js
+++ test/version-control-test/below10/cypress/plugins/index.js
@@ -20,5 +20,3 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
-
-require('@smartbear/visualtest-cypress')(module)
