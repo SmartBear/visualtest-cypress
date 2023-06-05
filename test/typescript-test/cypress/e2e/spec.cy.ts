2fe0f539b69de0e53ba32f190354890ce4554f45
trevor.nelms <trevor.nelms@smartbear.com>
2023-06-05 15:55:59 -0600

    SBVT-1510: updating .gitignore

--- test/typescript-test/cypress/e2e/spec.cy.ts
+++ test/typescript-test/cypress/e2e/spec.cy.ts
@@ -1,10 +1,13 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    // @ts-ignore
     cy.sbvtCapture('webkit-fullpage')
+    // @ts-ignore
     cy.sbvtCapture('webkit-viewport', {
       capture: 'viewport'
     })
+    // @ts-ignore
     cy.get('.banner').sbvtCapture('webkit-element')
   })
 })
