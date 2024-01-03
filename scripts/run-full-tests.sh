cea18bfb181ec89bad3dc79a637e662c720d5ae9
trevornelms <trevornelms@outlook.com>
2024-01-03 11:55:21 -0700

    SBVT-2181: adding testGroupName functionality

--- scripts/run-full-tests.sh
+++ scripts/run-full-tests.sh
@@ -8,11 +8,14 @@ BLUE=$(tput setaf 4)
 #MAGENTA=$(tput setaf 5)
 RESET=$(tput sgr0)
 
+
+scripts/utils/check-npm-updates.sh
+
 echo "${BLUE}Navigating to the 'test' directory...${RESET}"
 cd test || exit 1
 
 
 echo "${BLUE}Running Cypress tests...${RESET}"
-npx cypress run -q -s 'cypress/e2e/quick*'
+npx cypress run -q -s 'cypress/e2e/test-spec/*'
 
 cd ..
